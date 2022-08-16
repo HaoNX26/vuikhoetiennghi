5b71afa28dbc4136f0f77821a4bc1b8c6633f7d5
HaoNX <haonx@gmail.com>
2022-08-16 19:57:33 +0700

    update code

---
+++ JoinReward/Models/SubmitCustomerWin/B_STEP_CUSTOMER_SUBMIT.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JoinReward.Models.SubmitCustomerWin
+{
+    [Table("B_STEP_CUSTOMER_SUBMIT")]
+    public class B_STEP_CUSTOMER_SUBMIT
+    {
+        [Key]
+        public long ID { get; set; }
+        public long CUSTOMER_SUBMIT_ID { get; set; }
+        public long STEP_ID { get; set; }
+
+        public string NOTE_ERROR { get; set; }
+    }
+}
